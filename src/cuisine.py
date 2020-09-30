0195ba6cea2fe16a28de227af4c9cf53b21524ad
Adam Jakubowski <ajakubo1@gmail.com>
2020-09-30 11:31:43 +0200

    goddamit, I knew that test suite was not complete

--- src/cuisine.py
+++ src/cuisine.py
@@ -855,7 +855,7 @@ def file_link(source, destination, symbolic=True, mode=None, owner=None, group=N
 def file_base64(location):
 	"""Returns the base64-encoded content of the file at the given location."""
 	if fabric.api.env[OPTION_HASH] == "python":
-		return run("cat {0} | python -c 'import sys;sys.stdout.write(sys.stdin.read())'".format(shell_safe((location))))
+		return run("cat {0} | python -c 'import sys;sys.stdout.write(sys.stdin.read().encode(\"utf-8\"))'".format(shell_safe((location))))
 	else:
 		return run("cat {0} | openssl base64".format(shell_safe((location))))
 
